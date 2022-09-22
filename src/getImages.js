b781129af83d48ef185cd96855b5afff0674c434
Scumsy <dmitry.koschavka@gmail.com>
2022-09-22 23:27:19 +0300

    almost done

--- src/getImages.js
+++ src/getImages.js
@@ -5,23 +5,17 @@ const options = `&image_type=photo&orientation=horizontal&safesearch=true`;
 const KEY = '29997517-93cd94c02db4cfa0c008c948f';
 const BASE_URL = 'https://pixabay.com/api/';
 
-function getSearchRequest(input) {
-  return axios.get(`${BASE_URL}?key=${KEY}&q=${input}${options}`).then(res => {
-    console.log(res);
-  });
+let images = [];
+async function getSearchRequest(input, pageNumber) {
+  try {
+    const res = await axios.get(
+      `${BASE_URL}?key=${KEY}&q=${input}${options}&page=${pageNumber}&per_page=40`
+    );
+    images = res.data.hits;
+    return images;
+  } catch {
+    Notiflix.Notify.failure("We're sorry, something went wrong.");
+  }
 }
+
 export { getSearchRequest };
-
-// function fetchCountries(input) {
-//   return fetch(`https://restcountries.com/v2/name/${input}?${options}`).then(
-//     res => {
-//       //   console.log(res);
-//       return res.json();
-//     }
-//   );
-// }
-
-// export { fetchCountries };
-
-// https://restcountries.com/v2/all?fields=name,capital,currencies
-// `https://restcountries.com/v2/name/${input}`;
--- src/index.js
+++ src/index.js
@@ -5,36 +5,80 @@ import { getSearchRequest } from './getImages';
 import Notiflix from 'notiflix';
 import axios from 'axios';
 
-let inputForSearch;
+let images = [];
+let inputForSearch = '';
+let pageNumber = 1;
+const gallery = document.querySelector('.gallery');
+const searchForm = document.querySelector('#search-form');
+const loadMoreBtn = document.querySelector('.load-more');
 
-// const searchForm = document.querySelector('#search-form');
-// console.log(searchForm);
-// searchForm.addEventListener('submit', getSearchRequest(searchForm[0].value));
+searchForm.addEventListener('input', e => {
+  return (inputForSearch = e.target.value);
+});
 
-// console.log(searchForm);
+searchForm.addEventListener('submit', e => {
+  e.preventDefault();
 
-// function getDataSearch(data) {
-//   inputForSearch = data.target.value.trim();
-//   if (inputForSearch !== '') {
-//     return getSearchRequest(inputForSearch)
-//       .then(displayCountry)
-//       .catch(error => {});
-//   }
-// }
+  getSearchRequest(inputForSearch, pageNumber).then(images => {
+    console.log(images);
+    if (images.length === 0) {
+      Notiflix.Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      gallery.innerHTML = '';
+      renderImageList(images);
+    }
+  });
+});
 
-// ---------------Search form-----------------
-const inputData = document.querySelector('.input-field');
-console.log(inputData);
-inputData.addEventListener('input', showValue(inputData));
+loadMoreBtn.addEventListener(
+  'click',
+  (e = async () => {
+    pageNumber += 1;
+    const response = await getSearchRequest(inputForSearch, pageNumber);
 
-function showValue(input) {
-  inputForSearch = input.textContent;
-  console.log(inputForSearch);
+    return renderImageList(response);
+  })
+);
+
+function makeGalleryMarkup(url, tag, likes, views, comments, downloads) {
+  const markup = `<div class="photo-card">
+  <img src="${url}" alt="${tag}" loading="lazy" width='300' height='200'/>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      <span>${likes}</span>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      <span>${views}</span>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      <span>${comments}</span>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      <span>${downloads}</span>
+    </p>
+  </div>
+</div>`;
+  return markup;
 }
 
-// ------------------- SUBMIT BTN ----------------------
-const submitBtn = document.querySelector('.submit-btn');
-submitBtn.addEventListener('submit', e => {
-  e.preventDefault();
-  return getSearchRequest(inputForSearch);
-});
+function renderImageList(images) {
+  for (let i = 0; i < images.length; i += 1) {
+    const imageList = makeGalleryMarkup(
+      images[i].webformatURL,
+      images[i].tags,
+      images[i].likes,
+      images[i].views,
+      images[i].comments,
+      images[i].downloads
+    );
+
+    gallery.insertAdjacentHTML('beforeend', imageList);
+    loadMoreBtn.classList.remove('is-hidden');
+  }
+}
